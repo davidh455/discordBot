608118206dd9a0ca58974b7bc845177d4c05787d
davidh455 <davidhoang455@gmail.com>
2024-12-03 10:13:51 -0800

    add comments

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+# os library to use environment variables
+# discord and ec2_metadata (They allow us to interact with AWS EC2 & Discord)
+# dotenv so we can import the .env file into python
+# random to generate random numbers and sequences
+
 import discord
 import os
 import random
 from ec2_metadata import ec2_metadata
 from dotenv import load_dotenv
 
+# load the env file
 load_dotenv("token.env")
 
+# set the client to the discord client
 client = discord.Client()
 
+# get the otken from the env file
 token = os.getenv('TOKEN')
 
-
+#Ensure that the bot has the proper gateway intents, otherwise we can't read user messages
 intents = discord.Intents.default()
 intents.message_content = True  # Make sure the bot has the proper permissions
 intents.messages = True
@@ -31,7 +39,8 @@ async def on_message(message):
 
     if message.author == client.user:
         return
-
+# Locates the channel and provides a response only in that channel
+# Some features are responsing to hi or hello, bye, tell me a joke, and provides the ec2 meta data (region, IP, and availability zone)
     if channel == "bot":
         if user_message.lower() == "hello" or user_message.lower() == "hi":
             await message.channel.send(f'Hello {username}')
